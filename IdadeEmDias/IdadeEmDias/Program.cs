90c7cb94d686e42cef2bb341204e19bf4fb9a106
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:04:46 -0300

    IdadeEmDias

---
+++ IdadeEmDias/IdadeEmDias/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int idade, ano, meses, dias, resto;
+
+        idade = int.Parse(Console.ReadLine());
+
+        ano = idade / 365;
+        resto = idade % 365;
+        meses = resto / 30;
+        resto %= 30;
+        dias = resto;
+
+        Console.WriteLine(ano + " ano(s)");
+        Console.WriteLine(meses + " mes(es)");
+        Console.WriteLine(dias + " dia(s)");
+
+    }
+}
