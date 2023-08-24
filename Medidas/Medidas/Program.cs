f79132dfcb1784a135ec89a3cfc85fc793907df7
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:09:51 -0300

    Medidas

---
+++ Medidas/Medidas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double altura, Base, area, perimetro, diagonal;
+
+        Base = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        area = Base * altura;
+
+        perimetro = 2 * (Base + altura);
+
+        diagonal = Math.Sqrt(Math.Pow(Base, 2.0) + Math.Pow(altura, 2.0));
+
+        Console.WriteLine("AREA = " + area.ToString("F4"), CultureInfo.InvariantCulture);
+        Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4"), CultureInfo.InvariantCulture);
+        Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4"), CultureInfo.InvariantCulture);
+    }
+}
