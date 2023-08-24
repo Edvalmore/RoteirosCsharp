70a2a5d915e08b1610a10a53c740e563c6a7ce5b
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:16:32 -0300

    Operadora

---
+++ Operadora/Operadora/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int minutos;
+        double valor = 50.0;
+
+        minutos = int.Parse(Console.ReadLine());
+
+        if (minutos < 100)
+        {
+            Console.WriteLine("Valor a pagar: R$ 50.00");
+        }
+        else
+        {
+            valor += (minutos - 100.0) * 2.0;
+            Console.WriteLine("Valor a pagar: R$ " + valor.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+
+    }
+}
