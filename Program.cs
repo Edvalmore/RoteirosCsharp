4b60d158c1a7931c7325fddb04a52ebde99fdcac
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:43:26 -0300

    Area
    Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
    a) a área do triângulo retângulo que tem A por base e C por altura.
    b) a área do círculo de raio C. (pi = 3.14159)
    c) a área do trapézio que tem A e B por bases e C por altura.
    d) a área do quadrado que tem lado B.
    e) a área do retângulo que tem lados A e B.
    Entrada
    O arquivo de entrada contém três valores com um dígito após o ponto decimal.
    
    Saída
    O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System;
+
+internal class Area
+{
+    private static void Main(string[] args)
+    {
+        double area, precoTotal;
+
+        Console.WriteLine("Informe as medidas de largura e comprimento do terreno:");
+        double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Informe o preço do metro quadrado do terreno");
+        double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        area = largura * comprimento;
+        precoTotal = preco * area;
+
+        Console.WriteLine("AREA = " +  area.ToString("F2"), CultureInfo.InvariantCulture);
+        Console.WriteLine("PRECO = " + precoTotal.ToString("F2"), CultureInfo.InvariantCulture);
+
+    }
+}
