84ff503082518175ccbc2450d00a3a737adf6673
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:02:46 -0300

    FormulaDeBhaskara

---
+++ FormulaDeBhaskara/FormulaDeBhaskara/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double delta, r1, r2;
+
+        string[] vet = Console.ReadLine().Split(' ');
+
+        double A = double.Parse(vet[0], CultureInfo.InvariantCulture);
+        double B = double.Parse(vet[1], CultureInfo.InvariantCulture);
+        double C = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+        delta = Math.Pow(B, 2.0) - 4 * A * C;
+
+        if (A == 0 || delta < 0.0) {
+
+            Console.WriteLine("Impossivel calcular");
+
+        } else {
+            r1 = (- B + Math.Sqrt(delta)) / (2.0 * A);
+            r2 = (- B - Math.Sqrt(delta)) / (2.0 * A);
+
+            Console.WriteLine("R1 = " + r1.ToString("F5", CultureInfo.InvariantCulture));
+            Console.WriteLine("R2 = " + r2.ToString("F5", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+}
