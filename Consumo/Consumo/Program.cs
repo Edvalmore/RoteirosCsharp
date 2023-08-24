97095b46ae0aaf5871b3a4aa764a087f6787e542
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:51:51 -0300

    Consumo

---
+++ Consumo/Consumo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int km;
+        double litros, consumo;
+
+        km = int.Parse(Console.ReadLine());
+        litros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        consumo = km /  litros;
+
+        Console.WriteLine(consumo.ToString("F3") + " km/l", CultureInfo.InvariantCulture);
+    }
+}
