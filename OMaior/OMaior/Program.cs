a61ad13673c41052b38412997f2a6f49b6cedf51
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:15:41 -0300

    OMaior

---
+++ OMaior/OMaior/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int maiorAB, maior;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        int n1 = int.Parse(vet[0]);
+        int n2 = int.Parse(vet[1]);
+        int n3 = int.Parse(vet[2]);
+
+        maiorAB = (n1 + n2 + Math.Abs(n1 - n2)) / 2;
+        maior = (maiorAB + n3 + Math.Abs(maiorAB - n3)) / 2;
+
+        Console.WriteLine(maior + " eh o maior");
+    }
+}
