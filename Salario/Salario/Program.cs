d830a0439e592890999bd98cefc8a06fc5df85fd
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:17:59 -0300

    Salario

---
+++ Salario/Salario/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, horasTrabalhadas;
+        double valorPorHora, salario;
+
+        num = int.Parse(Console.ReadLine());
+        horasTrabalhadas = int.Parse(Console.ReadLine());
+        valorPorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        salario = valorPorHora * horasTrabalhadas;
+
+        Console.WriteLine("NUMBER = " + num);
+        Console.WriteLine("SALARY = U$ " + salario.ToString("F2"), CultureInfo.InvariantCulture);
+    }
+}
