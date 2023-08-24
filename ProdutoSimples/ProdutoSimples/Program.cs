cc37987ae8e78192255805b291c61568a9330daf
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:17:16 -0300

    ProdutoSimples

---
+++ ProdutoSimples/ProdutoSimples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int a, b, PROD;
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+
+        PROD = a * b;
+
+        Console.WriteLine("PROD = " + PROD);
+    }
+}
