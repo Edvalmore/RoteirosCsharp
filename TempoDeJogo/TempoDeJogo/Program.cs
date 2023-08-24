53d2194b43e974a9108b898c6a186ab792d88f0b
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:20:32 -0300

    TempoDeJogo

---
+++ TempoDeJogo/TempoDeJogo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int HoraInicial, HoraFinal, HoraTotal;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        HoraInicial = int.Parse(vet[0]);
+        HoraFinal = int.Parse(vet[1]);
+
+        if (HoraInicial < HoraFinal)
+            HoraTotal = HoraFinal - HoraInicial;
+        else
+            HoraTotal = 24 - HoraInicial + HoraFinal;
+
+        Console.WriteLine("O JOGO DUROU " + HoraTotal + " HORA(S)");
+
+    }
+}
