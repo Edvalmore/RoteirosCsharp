cd8bd6d3e3b99e1fd42786fac2b2b1648e31edb1
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:18:45 -0300

    SalarioComBonus

---
+++ SalarioComBonus/SalarioComBonus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double SalarioFixo, TotalVendas, TotalReceber;
+
+        string nome = Console.ReadLine();
+        SalarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        TotalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        TotalReceber = TotalVendas * 0.15 + SalarioFixo;
+
+        Console.WriteLine("TOTAL = R$ " + TotalReceber.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
