7d015e803b8c6243e1b16d4b9bbe64ba70917be4
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:47:29 -0300

    AreaDoCirculo

---
+++ AreadoCirculo/AreadoCirculo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n = 3.14159, raio, area;
+
+        raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        area = n * (Math.Pow(raio, 2));
+
+        Console.WriteLine("A=" + area.ToString("F4"), CultureInfo.InvariantCulture);
+
+    }
+}
