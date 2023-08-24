bf40dc4cf76127acad3234db72b3013418fa2023
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:06:47 -0300

    Lanche

---
+++ Lanche/Lanche/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int codigo, quantidade;
+        double valor, total; 
+
+        string[] vet = Console.ReadLine().Split(' ');
+        codigo = int.Parse(vet[0]);
+        quantidade = int.Parse(vet[1]);
+
+        switch (codigo)
+        {
+            case 1:
+                valor = 4.0;
+                total = valor * quantidade;
+                Console.WriteLine("Total: R$ " + total.ToString("F2"));
+                break;
+            
+            case 2:
+                valor = 4.5;
+                total = valor * quantidade;
+                Console.WriteLine("Total: R$ " + total.ToString("F2"));
+                break;
+
+            case 3:
+                valor = 5.0;
+                total = valor * quantidade;
+                Console.WriteLine("Total: R$ " + total.ToString("F2"));
+                break;
+
+            case 4:
+                valor = 2.0;
+                total = valor * quantidade;
+                Console.WriteLine("Total: R$ " + total.ToString("F2"));
+                break;
+
+            case 5:
+                valor = 1.50;
+                total = valor * quantidade;
+                Console.WriteLine("Total: R$ " + total.ToString("F2"));
+                break;
+
+        }
+
+    }
+}
