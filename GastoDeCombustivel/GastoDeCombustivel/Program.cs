1340a50a42382b2e150e9a4fa5ac592c57f644e8
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:03:34 -0300

    GastoDeCombustivel

---
+++ GastoDeCombustivel/GastoDeCombustivel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int horas, kmPorHora;
+        double litros;
+
+        horas = int.Parse(Console.ReadLine());
+        kmPorHora = int.Parse(Console.ReadLine());
+
+        litros = (horas * kmPorHora) / 12.0;
+
+        Console.WriteLine(litros.ToString("F3"), CultureInfo.InvariantCulture);
+    }
+}
