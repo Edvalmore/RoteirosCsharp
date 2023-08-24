f8b6eb11a726b120a53f92f0ca2c052d29ceb792
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:11:25 -0300

    MenorDosNumeros

---
+++ MenorDosNumeros/MenorDosNumeros/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1, n2, n3;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        n1 = int.Parse(vet[0]);
+        n2 = int.Parse(vet[1]);
+        n3 = int.Parse(vet[2]);
+
+        if (n1 < n2 && n1 < n3)
+            Console.WriteLine("MENOR = " + n1);
+        else if (n2 < n1 && n2 < n3)
+            Console.WriteLine("MENOR = " + n2);
+        else
+            Console.WriteLine("MENOR = " + n3);
+
+    }
+}
