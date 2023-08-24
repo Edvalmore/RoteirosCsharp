a2b60d6ae1924d86b3844908d9a8cb72a990e661
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:13:18 -0300

    NotasAlunos

---
+++ NotasAlunos/NotasAlunos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double nota1, nota2, soma;
+
+        nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        soma = nota1 + nota2;
+
+        if (soma > 60.00) {
+            Console.WriteLine("NOTA FINAL = " + soma);
+        } else
+        {
+            Console.WriteLine("NOTA FINAL = " + soma);
+            Console.WriteLine("REPROVADO");
+        }
+
+    }
+}
