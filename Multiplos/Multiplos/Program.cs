065a7309a0deb8d4d445ddfdc5a30ded8b24a10a
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:11:51 -0300

    Multiplos

---
+++ Multiplos/Multiplos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1, n2;
+
+        string[] vet = Console.ReadLine().Split(' ');
+        n1 = int.Parse(vet[0]);
+        n2 = int.Parse(vet[1]);
+
+        if (n1 %n2 == 0 || n2 %n1 == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+
+    }
+}
