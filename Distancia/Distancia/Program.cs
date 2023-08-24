91f86a638d30a448a9e2af66e7eed35200d80d76
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:57:02 -0300

    Distancia

---
+++ Distancia/Distancia/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int tempo, km;
+
+        km = int.Parse(Console.ReadLine());
+
+        tempo = km * 2;
+
+        Console.WriteLine(tempo + " minutos");
+
+    }
+}
