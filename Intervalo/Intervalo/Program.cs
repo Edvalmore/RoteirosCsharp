5f51c5e49484b2acc22a2038d048974d3270355f
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:06:22 -0300

    Intervalo

---
+++ Intervalo/Intervalo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        double intervalo;
+
+        intervalo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if(intervalo >= 0.0  && intervalo <= 25.0)
+        {
+            Console.WriteLine("Intervalo [0,25]");
+        } else 
+        if (intervalo > 25.0 && intervalo <= 50.0)
+        {
+            Console.WriteLine("Intervalo (25,50]");
+        } else
+        if (intervalo > 50.0 && intervalo <= 75.0)
+        {
+            Console.WriteLine("Intervalo (50,75]");
+        } else
+        if (intervalo > 75.0 && intervalo <= 100.0)
+        {
+            Console.WriteLine("Intervalo (75,100]");
+        } else
+        {
+            Console.WriteLine("Fora de intervalo");
+        }
+
+    }
+}
