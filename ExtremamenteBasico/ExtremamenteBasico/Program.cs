563a5ad25852983b1fb401f1791b4f5b18bb23ed
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:01:38 -0300

    ExtremamenteBasico

---
+++ ExtremamenteBasico/ExtremamenteBasico/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, X;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        X = A + B;
+
+        Console.WriteLine("Hello World!");
+    }
+}
