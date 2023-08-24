106425913e25d84c194d1ee3698c953b1b062a70
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:19:39 -0300

    SomaSimples

---
+++ SomaSimples/SomaSimples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int A, B, SOMA;
+
+        A = int.Parse(Console.ReadLine());
+        B = int.Parse(Console.ReadLine());
+
+        SOMA = A + B;
+
+        Console.WriteLine("SOMA = " + SOMA);
+    }
+}
