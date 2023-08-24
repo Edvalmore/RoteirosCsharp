b8ac8c3525db409f50af6a8116192104358db06c
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:52:56 -0300

    ConversaoDeTempo

---
+++ ConversaoDeTempo/ConversaoDeTempo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int segundosInt, horas, minutos, segundos;
+
+        segundosInt = int.Parse(Console.ReadLine());
+
+        horas = segundosInt / 3600;
+        minutos = (segundosInt - horas * 3600) / 60;
+        segundos = segundosInt - (horas * 3600 + minutos * 60);
+
+        Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+    }
+}
