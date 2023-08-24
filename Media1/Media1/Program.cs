57bab9bc4067a96a8a5263d20bfeb69b506f8269
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 11:07:40 -0300

    Media1

---
+++ Media1/Media1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double A, B, media;
+
+        A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        media = ((A * 3.5) + (B * 7.5)) / 11;
+
+        Console.WriteLine("MEDIA = " + media.ToString("F5"), CultureInfo.InvariantCulture);
+    }
+}
