8bf7b9de724e37b97fbfb4a8dc834863f99eb700
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-24 10:58:50 -0300

    Esfera

---
+++ Esfera/Esfera/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double raio, volume;
+
+        raio = double.Parse(Console.ReadLine());
+
+        volume = (4/3.0) * 3.14159 * Math.Pow(raio, 3);
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3", CultureInfo.InvariantCulture));
+    }
+}
