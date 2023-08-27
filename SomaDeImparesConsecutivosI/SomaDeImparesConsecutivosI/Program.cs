fbe447b2d39dc22d1bbdee618a4ed8f4a3369ba6
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 14:43:09 -0300

    SomaDeImparesConsecutivosI

---
+++ SomaDeImparesConsecutivosI/SomaDeImparesConsecutivosI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int inicio, fim;
+        int soma = 0;
+
+        if (x < y)
+        {
+            inicio = x;
+            fim = y;
+
+        }
+        else
+        {
+            inicio = y;
+            fim = x;
+        }
+
+        for (int i = inicio + 1; i < fim; i++)
+        {
+            if (i % 2 != 0)
+            {
+                soma += i;
+            }
+        }
+        Console.WriteLine(soma);
+    }
+}
