bc96862fe5f9b30f2fcda59ecad7c34e0c96b895
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 13:10:43 -0300

    Idades

---
+++ Idades/Idades/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double media, idade, soma, cont;
+        idade = int.Parse(Console.ReadLine());
+        soma = 0;
+        cont = 0;
+
+        while (idade > 0)
+        {
+            soma += idade;
+            cont++;
+            idade = int.Parse(Console.ReadLine());
+        }
+
+        media = soma / cont;
+
+        Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+    }
+}
