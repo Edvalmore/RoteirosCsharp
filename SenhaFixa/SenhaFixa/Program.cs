7191cef42380ee6caf2cf95d22464ff379c767c3
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 13:08:34 -0300

    SenhaFixa

---
+++ SenhaFixa/SenhaFixa/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int senha;
+        senha = int.Parse(Console.ReadLine());
+
+        while (senha != 2002)
+        {
+            Console.WriteLine("Senha Invalida");
+            senha = int.Parse(Console.ReadLine());
+
+        }
+
+        Console.WriteLine("Acesso Permitido");
+    }
+}
