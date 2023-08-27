90ccd0be9e4b3a9b187812b437429f8dceb2f43f
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 14:05:01 -0300

    MediaIdades

---
+++ MediaIdades/MediaIdades/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int cont;
+        double media, idade, soma;
+        idade = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+        cont = 0;
+        soma = 0.0;
+
+        if (idade >= 0.0)
+        {
+            while (idade > 0.0)
+            {
+                cont++;
+                soma += idade;
+                idade = double.Parse(Console.ReadLine(), CultureInfo.InstalledUICulture);
+            }
+            media = soma / cont;
+            Console.WriteLine(media.ToString("F2", CultureInfo.InstalledUICulture));
+        }
+        else
+            Console.WriteLine("impossivel calcular");
+    }
+}
