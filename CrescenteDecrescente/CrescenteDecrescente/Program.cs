eb08c8df2d9a4c3a68c3aaf1e09b9889eacb30d3
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 14:03:36 -0300

    CrescenteDecrescente

---
+++ CrescenteDecrescente/CrescenteDecrescente/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        int x = int.Parse(vet[0]);
+        int y = int.Parse(vet[1]);
+
+        while (x != y)
+        {
+            if (x > y)
+                Console.WriteLine("Decrescente");
+            else
+                Console.WriteLine("Crescente");
+
+            vet = Console.ReadLine().Split(' ');
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+        }
+
+
+    }
+}
