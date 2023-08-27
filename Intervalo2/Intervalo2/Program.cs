e31c999cfadf3db087d957de15a660ce56c835d1
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 15:02:00 -0300

    Intervalo2

---
+++ Intervalo2/Intervalo2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num, contIn, contOut;
+        int x = int.Parse(Console.ReadLine());
+        contIn = 0;
+        contOut = 0;
+
+        for (int i = 0; i < x; i++)
+        {
+            num = int.Parse(Console.ReadLine());
+
+            if (num >= 10 && num <= 20)
+                contIn++;
+            else 
+                contOut++;
+        }
+
+        Console.WriteLine(contIn + " in");
+        Console.WriteLine(contOut + " out");
+    }
+}
