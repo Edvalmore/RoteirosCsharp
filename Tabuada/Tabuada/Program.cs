4421647f089ee0412b25ef592783c4d3dd5197e6
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-27 14:02:31 -0300

    Tabuada

---
+++ Tabuada/Tabuada/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int multi;
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= 10; i++)
+        {
+            multi = i * n;
+            Console.WriteLine(i + " x " + n + " = " + multi);
+
+        }
+    }
+}
