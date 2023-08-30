ba9b8aa9387874abb16688d2cdb23b392d7df2fd
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-08-30 12:00:44 -0300

    ParOuImpar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            
+
+            for (int i = 0; i < N; i++) {
+                int num = int.Parse(Console.ReadLine());
+                
+                if (num == 0)
+                    Console.WriteLine("NULL");
+                    else 
+                if (num > 0 && num %2 == 0)
+                    Console.WriteLine("EVEN POSITIVE");
+                    else
+                if (num < 0 && num %2 == 0)
+                    Console.WriteLine("EVEN NEGATIVE");
+                    else
+                if (num > 0 && num %2 != 0)
+                    Console.WriteLine("ODD POSITIVE");
+                    else
+                    Console.WriteLine("ODD NEGATIVE");
+            }
+        }
+    }
+}
