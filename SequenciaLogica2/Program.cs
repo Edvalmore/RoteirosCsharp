142c03e40cf3d3f7a85e8b8611d289a5a2b5b88c
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-04 08:08:03 -0300

    SequenciaLogica2

---
+++ SequenciaLogica2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        int numLinha = int.Parse(vet[0]);
+        int numMax = int.Parse(vet[1]);
+        
+        for (int i = 1; i <= numMax; i++)
+        {
+            Console.Write(i);
+
+            if (i %numLinha == 0)
+                Console.WriteLine();
+                else
+                Console.Write(" ");
+        }
+    }
+}
