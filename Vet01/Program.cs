34e10ef5895e08d97207853ea85a80f5e1aa1773
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-05 10:45:36 -0300

    Vet01

---
+++ Vet01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = int.Parse(Console.ReadLine());
+        double[] Num = new double[N];
+
+        for (int i = 0; i < N; i++)
+        {
+            Num[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine(Num[i].ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
