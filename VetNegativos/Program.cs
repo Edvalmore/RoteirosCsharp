307b0481545d14b201c2190a9e6bcb4028798254
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-05 10:46:17 -0300

    VetNegativos

---
+++ VetNegativos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] vet = new int[n];
+        string[] vet1 = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i<n; i++) {
+            vet[i] = int.Parse(vet1[i]);
+        }
+
+        for (int i = 0; i<n; i++) {
+            if (vet[i] < 0)
+                Console.WriteLine(vet[i]);
+        }
+
+    }
+}
+
