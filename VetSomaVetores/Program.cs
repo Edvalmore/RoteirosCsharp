8a52411d5226b6beb8f466679c8d562360480eac
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-06 11:14:37 -0300

    VetSomaVetores

---
+++ VetSomaVetores/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] a = new int[n];
+        int[] b = new int[n];
+
+        string[] num = Console.ReadLine().Split(' ');
+        for (int i = 0; i < n; i++) {
+            a[i] = int.Parse(num[i]);
+        }
+
+        num = Console.ReadLine().Split(' ');
+        for (int i = 0; i<n; i++){
+            b[i] = int.Parse(num[i]);
+        }
+
+        int[] c = new int[n];
+        for (int i = 0; i<n; i++) {
+            c[i] = a[i] + b[i];
+            Console.Write(c[i] + " ");
+        }
+    }
+}
