98664ee53f6fd1833dbcd52be8415ef6e0bb75b6
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-06 10:55:36 -0300

    VetPares

---
+++ VetPares/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] vet = Console.ReadLine().Split(' ');
+        int[] num = new int[n];
+
+
+        int cont = 0;
+        for (int i = 0; i < n; i++)
+        {
+            num[i] = int.Parse(vet[i]);
+            if (num[i] % 2 == 0)
+            {
+                Console.Write(num[i] + " ");
+                cont++;
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine(cont);
+    }
+}
