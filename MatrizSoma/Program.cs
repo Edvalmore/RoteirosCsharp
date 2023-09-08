ed503e9226e967aca97c0a147377ee0996455396
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-08 09:58:17 -0300

    MatrizSoma

---
+++ MatrizSoma/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] num = Console.ReadLine().Split(' ');
+        int m = int.Parse(num[0]);
+        int n = int.Parse(num[1]);
+        int[,] matriz = new int[m,n];
+        int soma=0;
+
+        for (int i=0; i<m; i++) {
+            string[] ler = Console.ReadLine().Split(' ');
+            for (int j=0; j<n; j++) {
+                matriz[i,j] = int.Parse(ler[j]);
+            }
+        }
+    
+        int[] tela = new int[m];
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++){
+                soma += matriz[i,j];
+            }
+            tela[i] = soma;
+            soma=0;
+        }
+
+        for(int i=0; i<m; i++) {
+            Console.WriteLine(tela[i]);
+        }
+
+    }
+}
