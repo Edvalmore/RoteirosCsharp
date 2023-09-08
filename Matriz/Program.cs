64777c53b2f40dee3a9f154732deec62753db6e0
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-08 08:23:22 -0300

    Matriz

---
+++ Matriz/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] num = Console.ReadLine().Split(' ');
+        int m = int.Parse(num[0]);
+        int n = int.Parse(num[1]);
+        int[,] matriz = new int[m, n];
+
+        for (int i = 0; i < m; i++)
+        {
+            string[] ler = Console.ReadLine().Split(' ');   
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = int.Parse(ler[j]);
+            }
+        }
+
+        for (int i=0; i<m; i++) {
+            for (int j = 0; j<n; j++){
+                Console.Write(matriz[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
