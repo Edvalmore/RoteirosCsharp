b8c186deb0ed839417ab6ad4b2c709c6e4c81570
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-14 09:57:59 -0300

    Classe06

---
+++ Classe06/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Classe06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno x = new Aluno();
+            Console.Write("Digite o nome do aluno: ");
+            x.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite as três notas do aluno:");
+            x.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            System.Console.WriteLine("Nota final: " + x.NotaFinal().ToString("F2",CultureInfo.InvariantCulture));
+
+            if(x.NotaFinal() > 60)
+                Console.WriteLine("Aprovado");
+                else {
+                    Console.WriteLine("Reprovado");
+                    Console.Write("Faltaram " + x.Pontos().ToString("F2", CultureInfo.InvariantCulture) + " pontos");
+                }
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+
+        public double Pontos() {
+            return 60.0 - NotaFinal();
+        }
+    }
+}
