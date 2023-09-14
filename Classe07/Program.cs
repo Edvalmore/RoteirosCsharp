31c5f54a88287feaaadd00ed397b591ab6b67a11
Ed Oliveira <102634190+Edvalmore@users.noreply.github.com>
2023-09-14 11:38:04 -0300

    Classe07

---
+++ Classe07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Classe07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversorMoeda x = new ConversorMoeda();
+            Console.Write("Qual é a cotação do dólar? ");
+            x.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.Write("Quantos dólares você vai cotar? ");
+            x.Qnt = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            System.Console.WriteLine("Valor a ser pago em reais = " + x.ValorTotal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+
+
+    class ConversorMoeda {
+        public double Dolar;
+        public double Qnt;
+        public double ValorTotal() {
+            return Dolar * Qnt * 0.06 + Dolar * Qnt;
+        }
+
+    }
+}
